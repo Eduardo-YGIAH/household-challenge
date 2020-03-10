e5e444ad6e294b669d9219426deb80efbb749a90
Eduardo Neto <edu.neto@icloud.com>
2020-03-10 15:46:52 +0000

    Landing page logo and navegation

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Nav.scss';
 
 export default function Nav() {
   return (
     <div className='nav-container'>
       <div className='nav-container-links'>
-        <div className='logo'>LOGO</div>
-        <div className='nav-link'>About</div>
+        <div className='logo'>
+          <Link to='/'>
+            <img
+              src='https://res.cloudinary.com/ygiah/image/upload/v1583849791/Household-Challenge/icon.png'
+              alt='logo'
+              className='logo-img'
+            />
+          </Link>
+        </div>
+        <div className='nav-link'>
+          <Link to='/about'>About</Link>
+        </div>
       </div>
     </div>
   );
