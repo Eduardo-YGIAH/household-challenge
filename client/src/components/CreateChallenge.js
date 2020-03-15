61d39961209a1b8b3cb100f95ddc638e43f6dd25
Eduardo Neto <edu.neto@icloud.com>
2020-03-15 16:36:33 +0000

    page design

---
+++ client/src/components/CreateChallenge.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useContext } from 'react';
+import * as auth from '../helperFunctions/auth';
+import { navigate } from '@reach/router';
+import Button from './Button';
+import { UserContext } from '../context/UserContext';
+
+export default function CreateChallenge() {
+  const { user, setUser } = useContext(UserContext);
+  useEffect(() => {
+    if (!auth.isAuthenticated()) {
+      navigate('/login');
+    }
+    if (!user.isAuthenticated && auth.isAuthenticated()) {
+      const persistUser = auth.isAuthenticated();
+      setUser(persistUser);
+    }
+  });
+
+  const btnCreateChallenge = {
+    label: 'Create Challenge',
+    link: '#',
+    onclick: () => {},
+    style: 'large',
+  };
+  const btnCreateTask = {
+    label: 'Create Task',
+    link: '#',
+    onclick: () => {},
+    style: 'btn',
+  };
+
+  return (
+    <div className='container'>
+      <h1>Create a Challenge</h1>
+      <h3>Set Up the Start and End Date</h3>
+      <div className=''>Date Range Component here</div>
+      <h2 className='sub-heading'>Tasks:</h2>
+      <Button btn={btnCreateTask} />
+      <Button btn={btnCreateChallenge} />
+    </div>
+  );
+}
