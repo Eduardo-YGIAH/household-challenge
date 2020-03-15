42bdbf2c1c413b260ebd6f3c66986d0feb0a71e5
Eduardo Neto <edu.neto@icloud.com>
2020-03-15 10:11:17 +0000

    backend fix for image uploader to cloudinary

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const fileupload = require('express-fileupload');
 // var path = require('path');
 const connection = require('./config/db/dbConnection');
 
 const app = express();
 
+app.use(
+  fileupload({
+    useTempFiles: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
