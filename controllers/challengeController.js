b04289fd01a7308e6d0ec52354c5c899adf0cb3b
Nilesh Patel <npatel224@gmail.com>
2020-03-15 15:19:17 +0000

    routes updated ignore branch name

--- controllers/challengeController.js
+++ controllers/challengeController.js
@@ -1,18 +1,16 @@
-const Challenge = require('../models/Challenge.js');
+const { Challenge } = require('../models/Challenge.js');
 
 exports.create_challenge = async (req, res) => {
 
 	try {
-		
-		console.log(req.household._id);
-
 		const challenge = new Challenge({
 			...req.body,
-			householdId: req.household._id
+			householdId: req.body.householdId
 		});
 		await challenge.save();
 		res.status(201).send({ challenge });
 	} catch (error) {
+		// console.log(error);
 		res.status(400).send(error);
 	}
 };
@@ -24,13 +22,12 @@ exports.get_challenge = async (req, res) => {
 		const _id = req.params.id;
 		const challenge = await Challenge.findOne(
 			{ _id, 
-				householdId: req.household._id 
+				householdId: req.body.householdId
 			});
 
 		if (!challenge) {
 			return res.status(404).send();
 		}
-
     res.send(challenge);
         
 	} catch (error) {
@@ -46,33 +43,34 @@ exports.all_challenges = async (req, res) => {
 			return res.status(404).send();
 		}
 		res.send(allChallenges);
-	} catch (e) {
-		res.status(500).send();
+	} catch (error) {
+		res.status(500).send(error);
 	}
 }
 
 exports.update_challenge = async (req, res) => {
 	try {
-		await Challenge.finedOneAndUpdate(
+		await Challenge.findOneAndUpdate(
 			{ _id: req.params.id} ,
 			{ $set: req.body }
 		);
 		res.status(200).send();
-	} catch (e) {
-		res.status(500).send();
+	} catch (error) {
+		// console.log(error);
+		res.status(500).send(error);
 	}
 };
 
 exports.delete_challenge = async (req, res) => {
   try {
-    const removeChallenge = await Challenge.findOneAndDelete({ _id: req.params.id, householdId: req.household._id });
+    const deleteChallenge = await Challenge.findOneAndDelete({ _id: req.params.id, householdId: req.body.householdId });
 
-    if (!removeChallenge) {
+    if (!deleteChallenge) {
       res.status(404).send();
     }
 
-    res.send(removeChallenge);
-  } catch (e) {
-    res.status(500).send();
+    res.send(deleteChallenge);
+  } catch (error) {
+    res.status(500).send(error);
   }
 };
--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,9 +1,9 @@
-const Task = require('../models/Tasks');
+const { Task } = require('../models/Tasks');
 
 exports.create_task = async (req, res) => {
 	const task = new Task({
 		...req.body,
-		_challengeId: req.challenge._id
+		challengeId: req.body.challengeId
 	});
 
 	try {
@@ -19,13 +19,11 @@ exports.get_task = async (req, res) => {
 
 	try {
 		const task = await Task.findOne({ 
-			_id, _challengeId: req.challenge.id 
+			_id, challengeId: req.body.challengeId 
 		});
-
 		if (!task) {
 			return res.status(404).send();
 		}
-	
 		res.send(task);
 	} catch (error) {
 		res.status(500).send(error);
@@ -34,54 +32,57 @@ exports.get_task = async (req, res) => {
 
 exports.all_tasks = async (req, res) => {
 	try {
-		const allTasks = await Task.findAll({});
+		const tasks = await Task.find({});
 
-		if(!allTasks) {
+		if(!tasks) {
 			return res.status(404).send();
 		}
-		res.send(allTasks);
+		res.send({ tasks });
 	} catch (error) {
+		// console.log(error)
 		res.status(500).send(error);
 	}
 }
 
 exports.update_task = async (req, res) => {
-	const { title, description, checkListItems, taskValue, image } = req.body.data;
+	const { title, description, checkListItems, taskValue, image } = req.body;
 	try {
-		await Task.finedOneAndUpdate(
+		await Task.findOneAndUpdate(
 			{ 
 				_id: req.params.id,
-				_userId: req.user.id
+				challengeId: req.body.challengeId
 			},
 			{ $set: {
-					title, 
+					title,
 					description,
 					checkListItems,
 					taskValue,
 					image
-				} 
+				}
 			}
 		);
+		console.log(req.body)
 		res.status(200).send();
 	} catch (error) {
+		console.log(error)
 		res.status(500).send(error);
 	}
 };
 
 exports.delete_task = async (req, res) => {
   try {
-    const removeTask = await Task.findOneAndDelete(
+    const deleteTask = await Task.findOneAndDelete(
 			{ 
 				_id: req.params.id,
-				_challengeId: req.challenge._id
+				challengeId: req.body.challengeId
 			}
 		);
 
-    if (!removeTask) {
+    if (!deleteTask) {
       res.status(404).send();
     }
-
-    res.send(removeTask);
+		res.send(deleteTask);
+		
   } catch (error) {
     res.status(500).send(error);
   }
