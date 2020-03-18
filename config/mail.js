c578acc56185de9da3546ab509b78adb269f8a80
Nilesh Patel <npatel224@gmail.com>
2020-03-18 13:26:34 +0000

    invite route modified

--- config/mail.js
+++ config/mail.js
@@ -1,35 +1,19 @@
 require('dotenv').config();
+const mailgunLoader = require("mailgun-js");
 
-const nodemailer = require('nodemailer');
-const mailGun = require('nodemailer-mailgun-transport');
+let mailgun = mailgunLoader({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN
+});
 
-//configure transportor options
-const auth = {
-  auth: {
-    api_key: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN,
-  },
-};
-
-const transporter = nodemailer.createTransport(mailGun(auth));
-
-const sendMail = (email, subject, text, cb) => {      //(email, subject, text, cb)
-  const mailOptions = {
-    from: email,
-    to: process.env.MY_EMAIL,
+const sendMail = (to, from, subject, content) => {
+  let mailOptions = {
+    to,
+    from,
     subject,
-    text,
+    text: content
   };
+  return mailgun.messages().send(mailOptions);
+}
 
-  transporter.sendMail(mailOptions, function(err, data) {
-    if (err) {
-      // console.log("error")
-      return cb(err, null);
-    } else {
-      return cb(null, data);
-      // console.log('Invitation sent!!');
-    }
-  });
-};
-
-module.exports = sendMail;
+module.exports = { sendMail };
