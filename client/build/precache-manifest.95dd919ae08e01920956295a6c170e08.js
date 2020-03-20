ddd39f0e58e36a531142d2df04ff7584aa68c03e
Eduardo Neto <edu.neto@icloud.com>
2020-03-20 23:47:35 +0000

    build

---
+++ client/build/precache-manifest.95dd919ae08e01920956295a6c170e08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bece28b3793e8d32ddc3466cce354d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c9efe9fad2823096497",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "4d877d8f38e35c42506d",
+    "url": "/static/css/main.eacfd2c4.chunk.css"
+  },
+  {
+    "revision": "7c9efe9fad2823096497",
+    "url": "/static/js/2.b04abc66.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.b04abc66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d877d8f38e35c42506d",
+    "url": "/static/js/main.8e1c3760.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
